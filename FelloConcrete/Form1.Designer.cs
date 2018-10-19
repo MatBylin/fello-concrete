bef75cf47bba75ae8516e50cefdadfc65ceb4c0a
MatBylin <bylinsk@gmail.com>
2018-10-19 10:44:03 +0200

    Add UI Design

--- FelloConcrete/Form1.Designer.cs
+++ FelloConcrete/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonZginanie = new System.Windows.Forms.Button();
+            this.buttonScinanie = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.buttonScinanie);
+            this.panel1.Controls.Add(this.buttonZginanie);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(208, 537);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(86)))), ((int)(((byte)(136)))));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(208, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(674, 37);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(86)))), ((int)(((byte)(136)))));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(304, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(170, 51);
+            this.panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(8, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "kalkulatoryEC.pl";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 26);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonZginanie
+            // 
+            this.buttonZginanie.FlatAppearance.BorderSize = 0;
+            this.buttonZginanie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonZginanie.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonZginanie.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonZginanie.Image = ((System.Drawing.Image)(resources.GetObject("buttonZginanie.Image")));
+            this.buttonZginanie.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonZginanie.Location = new System.Drawing.Point(25, 133);
+            this.buttonZginanie.Name = "buttonZginanie";
+            this.buttonZginanie.Size = new System.Drawing.Size(180, 65);
+            this.buttonZginanie.TabIndex = 3;
+            this.buttonZginanie.Text = "     Zginanie EC2";
+            this.buttonZginanie.UseVisualStyleBackColor = true;
+            // 
+            // buttonScinanie
+            // 
+            this.buttonScinanie.FlatAppearance.BorderSize = 0;
+            this.buttonScinanie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonScinanie.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonScinanie.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonScinanie.Image = ((System.Drawing.Image)(resources.GetObject("buttonScinanie.Image")));
+            this.buttonScinanie.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonScinanie.Location = new System.Drawing.Point(25, 201);
+            this.buttonScinanie.Name = "buttonScinanie";
+            this.buttonScinanie.Size = new System.Drawing.Size(180, 65);
+            this.buttonScinanie.TabIndex = 4;
+            this.buttonScinanie.Text = "     Ścinanie EC2";
+            this.buttonScinanie.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(882, 537);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonScinanie;
+        private System.Windows.Forms.Button buttonZginanie;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
